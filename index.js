f55e1ab288cb14d0de2e63807d57fd57d23f7104
Daniyar <dkurbanov7639@gmail.com>
2023-01-21 22:06:15 +0500

    init project

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //document.getElementById('count').innerHTML =5 ;
 //let count = document.getElementById('count') ;
-let count=0;
+/*let count=0;
 let getEl = document.getElementById('count')
 function increment () {
      count += 1
@@ -12,4 +12,42 @@ let saveEl = document.getElementById('save-el');
 function save () {
      let countStr = count + " - "
      saveEl.innerHTML += countStr
-}
+     getEl.textContent = 0
+     count = 0
+}*/
+
+let count1 = 0;
+let scoreEl = document.getElementById('score1')
+function plus1() {
+    count1 +=1
+    scoreEl.innerHTML = count1;
+    
+} ;
+function plus2() {
+    count1 +=2
+    scoreEl.innerHTML = count1;
+    
+} ;
+function plus3() {
+     count1 +=3
+     scoreEl.innerHTML = count1;
+     
+ } ;
+
+ let count = 0
+ let scoreEl2 = document.getElementById('score2')
+function plus11() {
+    count +=1
+    scoreEl2.innerHTML = count;
+    
+} ;
+function plus22() {
+    count +=2
+    scoreEl2.innerHTML = count;
+    
+} ;
+function plus33() {
+     count +=3
+     scoreEl2.innerHTML = count;
+     
+ } ;
